21ec61f8f2fce4f76a797d6da73bc7aa3b23a179
majubs-mymellow <mymellow00@gmail.com>
2025-10-11 16:20:23 -0300

    ......

--- A06/script.js
+++ A06/script.js
@@ -1,6 +1,5 @@
 //Função Principal de validação que vai ser chamada pelo evento oninput do HTML
-function validarEmail() 
-{
+function validarCadastro() {
 
 
     //Passo 1: capturar os elementos do DOM (Tela)
@@ -9,27 +8,35 @@ function validarEmail()
     const emailInput = document.getElementById('email')
     const mensagemErro = document.getElementById('mensagemErro')
     const btnEnviar = document.getElementById('btnEnviar')
+    const passwordConfirmationInput = document.getElementById('passwordConfirmation')
+    const passwordInput = document.getElementById('password')
 
     //Passo 2: Verificar o valor a ser validado
     const email = emailInput.value.trim() //.trim() remove espaços em brancos
+    const password = passwordInput.value;
+    const passwordConfirmation = passwordConfirmationInput.value;
+    
 
     //Validação para verificar se o formato do email está correto (@email.com)
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
     let isValido = false;
+    let isPasswordValid = false;
+    let isPasswordConfirmationValid = false; 
     //todo endereço de email que for digitado, será inválido,
     //até que a validação acuse que ele é válido.
 
 
     //Passo 3: Aplicar as regras de validação
-    if (email == "") {
+    //  EMAIL
+    if(email == ""){
         //Campo vazio: não é um erro, mas também não é válido para continuar
         mensagemErro.textContent = "Digite seu e-mail para continuar."
         //limpar as classes de sucesso/erro
         mensagemErro.className = "mensagem-status"
         isValido = false
-    } else if (emailRegex.test(email)) {
+    } else if (emailRegex.test(email)){
         //email válido
         mensagemErro.textContent = "Email válido!"
         mensagemErro.className = 'mensagem-status sucesso'
@@ -43,19 +50,33 @@ function validarEmail()
         isValido = false;
     }
 
+
+    //     SENHA
+    if(password.leght <= 6){
+        isPasswordValid = false;
+        mensagemErro.textContent = "A senha deve conter no mínimo seis caracteres."
+    } 
+
+    if(isPasswordValid && password == passwordConfirmation){
+        isPasswordValid = false;
+        mensagemErro.textContent = "As senhas devem coincidir"
+    }
+
+
+
+
+
     //Passo 4: Fazer o envio do formulário e exibir um alerta de sucesso
 
-    document.getElementById('cadastroForm').addEventListener('submit', function (event){
-        event.preventDefault();
-
+    document.getElementById('cadastroForm').addEventListener('submit', function (event){ event.preventDefault()
 
     //Se o botão Enviar estiver habilitado, significa que a validação passou
-    if (!document.getElementById('btnEnviar').disabled) {
+    if(!document.getElementById('btnEnviar').disabled){
         alert('Cadastro enviado com sucesso: ' + document.getElementById('email').value)
         //é aqui que fariamos uma chama para uma API, por exemplo
 
     }
-})
-}
+})}
+validarCadastro()
 
-validarEmail()
+
