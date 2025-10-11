8af133293620f0d60ff01ad2d84961c511147912
majubs-mymellow <mymellow00@gmail.com>
2025-10-11 14:14:54 -0300

    ....

--- A06/script.js
+++ A06/script.js
@@ -1,5 +1,6 @@
 //Função Principal de validação que vai ser chamada pelo evento oninput do HTML
-function validarEmail() {
+function validarEmail() 
+{
 
 
     //Passo 1: capturar os elementos do DOM (Tela)
@@ -16,19 +17,19 @@ function validarEmail() {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
-    let isValido = false; 
+    let isValido = false;
     //todo endereço de email que for digitado, será inválido,
     //até que a validação acuse que ele é válido.
 
 
     //Passo 3: Aplicar as regras de validação
-    if(email == ""){
+    if (email == "") {
         //Campo vazio: não é um erro, mas também não é válido para continuar
         mensagemErro.textContent = "Digite seu e-mail para continuar."
         //limpar as classes de sucesso/erro
         mensagemErro.className = "mensagem-status"
         isValido = false
-    } else if (emailRegex.test(email)){
+    } else if (emailRegex.test(email)) {
         //email válido
         mensagemErro.textContent = "Email válido!"
         mensagemErro.className = 'mensagem-status sucesso'
@@ -41,4 +42,20 @@ function validarEmail() {
         btnEnviar.disabled = true;
         isValido = false;
     }
+
+    //Passo 4: Fazer o envio do formulário e exibir um alerta de sucesso
+
+    document.getElementById('cadastroForm').addEventListener('submit', function (event){
+        event.preventDefault();
+
+
+    //Se o botão Enviar estiver habilitado, significa que a validação passou
+    if (!document.getElementById('btnEnviar').disabled) {
+        alert('Cadastro enviado com sucesso: ' + document.getElementById('email').value)
+        //é aqui que fariamos uma chama para uma API, por exemplo
+
+    }
+})
 }
+
+validarEmail()
