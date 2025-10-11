aee52d35cee101cb82bbca9a099eaac116485676
majubs-mymellow <mymellow00@gmail.com>
2025-10-11 11:46:47 -0300

    :DD

---
+++ A06/script.js
@@ -0,0 +1,30 @@
+//Função Principal de validação que vai ser chamada pelo evento oninput do HTML
+function validarEmail() {
+
+
+    //Passo 1: capturar os elementos do DOM (Tela)
+    //cria uma variável chamada emailInput, acessa o documento .html e "pega" o 
+    //elemento pelo id "email"
+    const emailInput = document.getElementById('email')
+    const mensagemErro = document.getElementById('mensagemErro')
+    const btnEnviar = document.getElementById('btnEnviar')
+
+    //Passo 2: Verificar o valor a ser validado
+    const email = emailInput.value.trim() //.trim() remove espaços em brancos
+
+    //Validação para verificar se o formato do email está correto (@email.com)
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+    let isvalido = false; 
+    //todo endereço de email que for digitado, será inválido,
+    //até que a validação acuse que ele é válido.
+
+
+    //Passo 3: Aplicar as regras de validação
+    if(email == ""){
+        //Campo vazio: não é um erro, mas também não é válido para continuar
+        mensagemErro.textContent = "Digite seu e-mail para continuar."
+    }
+
+}
